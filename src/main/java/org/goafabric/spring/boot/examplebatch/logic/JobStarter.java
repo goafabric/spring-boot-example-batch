fa4ebbe47cd95b99124cc26aa8455816371cb6a2
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-02 13:22:36 +0200

    postgres added

---
+++ src/main/java/org/goafabric/spring/boot/examplebatch/logic/JobStarter.java
@@ -0,0 +1,28 @@
+package org.goafabric.spring.boot.examplebatch.logic;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class JobStarter {
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job importUserJob;
+
+
+    @PostConstruct
+    public void start() throws JobParametersInvalidException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException {
+        jobLauncher.run(importUserJob, new JobParameters());
+    }
+}
