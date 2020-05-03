5e47bebba29a7adeedc0a09421eebcb9839a1682
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-03 13:14:41 +0200

    refactoring

--- src/main/java/org/goafabric/spring/boot/examplebatch/logic/GenericFileItemReader.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/logic/GenericFileItemReader.java
@@ -24,17 +24,21 @@ public class GenericFileItemReader<T> extends FlatFileItemReader<T> {
 
     @Override
     public void afterPropertiesSet() throws Exception {
+        this.setResource(new ClassPathResource(fileName));
+        this.setLineMapper(createLineMapper());
+        super.afterPropertiesSet();
+    }
+
+    private DefaultLineMapper<T> createLineMapper() {
         final DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer(",");
         tokenizer.setNames(fieldNames);
+
         final BeanWrapperFieldSetMapper<T> fieldSetMapper = new BeanWrapperFieldSetMapper<>();
         fieldSetMapper.setTargetType(classType);
 
         final DefaultLineMapper<T> lineMapper = new DefaultLineMapper<>();
         lineMapper.setLineTokenizer(tokenizer);
         lineMapper.setFieldSetMapper(fieldSetMapper);
-
-        this.setResource(new ClassPathResource(fileName));
-        this.setLineMapper(lineMapper);
-        super.afterPropertiesSet();
+        return lineMapper;
     }
 }
