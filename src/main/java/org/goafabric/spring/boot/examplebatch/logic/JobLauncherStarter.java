3996c0de3f9b07efcd41261744fe87d6f5db9c94
Andreas Mautsch <amautsch>
2021-04-03 10:23:02 +0200

    flyway removed

--- src/main/java/org/goafabric/spring/boot/examplebatch/logic/JobLauncherStarter.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/logic/JobLauncherStarter.java
@@ -3,17 +3,9 @@ package org.goafabric.spring.boot.examplebatch.logic;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
 
 @Component
 public class JobLauncherStarter {
@@ -32,5 +24,12 @@ public class JobLauncherStarter {
 
         jobLauncher.run(personCatalogJob, jobParameters);
         jobLauncher.run(toyCatalogJob, jobParameters);
+
+        /*
+        while (true) {
+            jobLauncher.run(personCatalogJob, new JobParameters());
+            jobLauncher.run(toyCatalogJob, new JobParameters());
+        }
+         */
     }
 }
