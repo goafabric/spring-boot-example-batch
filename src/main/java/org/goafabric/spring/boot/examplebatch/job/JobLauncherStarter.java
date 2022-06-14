16ed93b5ac4c581ac27979cc1536428a93855cd5
Andreas Mautsch <amautsch>
2022-06-14 14:33:48 +0200

    itemreader added

--- src/main/java/org/goafabric/spring/boot/examplebatch/job/JobLauncherStarter.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/job/JobLauncherStarter.java
@@ -8,10 +8,11 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
+@Component
 public class JobLauncherStarter implements CommandLineRunner {
     @Autowired
     private JobLauncher jobLauncher;
@@ -22,18 +23,24 @@ public class JobLauncherStarter implements CommandLineRunner {
     @Autowired
     private Job toyCatalogJob;
 
-    @Value("${spring.batch.rate:-1}")
-    private String batchRate;
+    @Value("${spring.batch.scheduler.enabled}")
+    private Boolean schedulerEnabled;
 
     @Override
     public void run(String... args) throws Exception {
-        if (batchRate.equals("-1")) {
+        if (!schedulerEnabled) {
             start();
             log.info("launched manually and wait 5 seconds");
             Thread.currentThread().join(5000);
         }
     }
 
+    @Scheduled(fixedRateString = "${spring.batch.rate}")
+    public void schedule() throws Exception {
+        if (schedulerEnabled) {
+            start();
+        }
+    }
     public void start() throws Exception {
         final JobParameters jobParameters = new JobParametersBuilder()
                 .addString("catalogVersion", "10").toJobParameters();
