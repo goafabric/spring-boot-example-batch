7b030a90534b5d0a855454ea4cfa99ebad63d175
Andreas Mautsch <amautsch>
2022-06-14 14:41:52 +0200

    job scheduler added

--- src/main/java/org/goafabric/spring/boot/examplebatch/job/JobLauncherStarter.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/job/JobLauncherStarter.java
@@ -8,11 +8,15 @@ import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@EnableScheduling
 public class JobLauncherStarter implements CommandLineRunner {
     @Autowired
     private JobLauncher jobLauncher;
@@ -26,25 +30,26 @@ public class JobLauncherStarter implements CommandLineRunner {
     @Value("${spring.batch.scheduler.enabled}")
     private Boolean schedulerEnabled;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Override
     public void run(String... args) throws Exception {
         if (!schedulerEnabled) {
-            start();
+            start(new JobParametersBuilder().addString("catalogVersion", "10").toJobParameters());
             log.info("launched manually and wait 5 seconds");
             Thread.currentThread().join(5000);
+            SpringApplication.exit(applicationContext, () -> 0);
         }
     }
 
-    @Scheduled(fixedRateString = "${spring.batch.rate}")
+    @Scheduled(fixedRateString = "${spring.batch.scheduler.rate}")
     public void schedule() throws Exception {
         if (schedulerEnabled) {
-            start();
+            start(new JobParameters());
         }
     }
-    public void start() throws Exception {
-        final JobParameters jobParameters = new JobParametersBuilder()
-                .addString("catalogVersion", "10").toJobParameters();
-
+    public void start(JobParameters jobParameters) throws Exception {
         jobLauncher.run(personCatalogJob, jobParameters);
         jobLauncher.run(toyCatalogJob, jobParameters);
     }
