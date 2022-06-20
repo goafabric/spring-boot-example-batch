ea7a163d27f9bde0e81e8e430cf5fc3f70b4bef9
Andreas Mautsch <amautsch>
2022-06-20 13:16:29 +0200

    hints moved

--- src/main/java/org/goafabric/spring/boot/examplebatch/person/PersonConfiguration.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/person/PersonConfiguration.java
@@ -1,20 +1,5 @@
 package org.goafabric.spring.boot.examplebatch.person;
 
-/*
- * Copyright 2021-2021 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
 import lombok.SneakyThrows;
@@ -35,12 +20,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.nativex.hint.TypeAccess;
+import org.springframework.nativex.hint.TypeHint;
 
 import javax.sql.DataSource;
 
-/**
- * @author Michael Minella
- */
+@TypeHint(types = {Person.class}, access = { TypeAccess.DECLARED_CONSTRUCTORS, TypeAccess.PUBLIC_METHODS })
 @Configuration(proxyBeanMethods = false) //needed for spring-native
 public class PersonConfiguration {
 
