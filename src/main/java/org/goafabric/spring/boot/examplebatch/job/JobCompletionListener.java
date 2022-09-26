fc87b1efd470c5ca73afe20353aa09af352f09ba
Andreas Mautsch <amautsch>
2022-09-26 16:24:24 +0200

    refactoring

--- src/main/java/org/goafabric/spring/boot/examplebatch/job/JobCompletionListener.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/job/JobCompletionListener.java
@@ -3,12 +3,12 @@ package org.goafabric.spring.boot.examplebatch.job;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
-public class JobCompletionListener extends JobExecutionListenerSupport {
+public class JobCompletionListener implements JobExecutionListener {
 
     @Override
     public void afterJob(JobExecution jobExecution) {
