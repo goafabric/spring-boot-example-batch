88c8978b725d4b47c649a96357410df747dd14a4
Andreas Mautsch <amautsch>
2022-10-31 09:08:33 +0100

    test added

--- src/test/java/org/goafabric/spring/boot/examplebatch/ApplicationIT.java
+++ src/test/java/org/goafabric/spring/boot/examplebatch/ApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class ApplicationIT {
 
     @Test
-    public void test() {
+    public void contextLoads() {
         //SpringApplication.run(Application.class, "");
     }
 }
