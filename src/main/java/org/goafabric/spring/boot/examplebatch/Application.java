0beb40cd994f89c84b15c016c18c12dd65edd6b3
Andreas Mautsch <amautsch>
2022-11-18 08:18:18 +0100

    fixes

--- src/main/java/org/goafabric/spring/boot/examplebatch/Application.java
+++ src/main/java/org/goafabric/spring/boot/examplebatch/Application.java
@@ -1,6 +1,5 @@
 package org.goafabric.spring.boot.examplebatch;
 
-import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
@@ -19,13 +18,6 @@ public class Application {
     static class ApplicationRuntimeHints implements RuntimeHintsRegistrar {
         @Override
         public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-            //pojos
-            hints.reflection().registerType(org.goafabric.spring.boot.examplebatch.domain.Person.class,
-                    MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);
-
-            hints.reflection().registerType(org.goafabric.spring.boot.examplebatch.domain.Toy.class,
-                    MemberCategory.INVOKE_DECLARED_CONSTRUCTORS, MemberCategory.INVOKE_DECLARED_METHODS);
-
             //resources
             hints.resources().registerPattern("catalogdata/*.csv");
         }
