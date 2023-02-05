ac9e79823f73cdaa17f8eeba1e6a3004ea6be7b4
Andreas Mautsch <amautsch>
2023-02-05 16:53:30 +0100

    changes

--- src/main/resources/db/migration/V2__toy.sql
+++ src/main/resources/db/migration/V2__toy.sql
@@ -2,15 +2,6 @@
 CREATE SCHEMA IF NOT EXISTS masterdata;
 
 -- catalog tables
-CREATE TABLE masterdata.person_catalog  (
-   	id varchar(36) not null
-		constraint pk_people
-			primary key,
-    catalog_version VARCHAR(20),
-
-    first_name VARCHAR(20),
-    last_name VARCHAR(20)
-);
 
 CREATE TABLE masterdata.toy_catalog (
    	id varchar(36) not null
